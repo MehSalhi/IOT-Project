bb4689ffc85d9fbe5cb0853d09fc3a9ca871eb7c
Mbayo <gmbayo11@gmail.com>
2023-05-23 14:43:16 +0200

    Add connection to wifi with WIFININA

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,54 @@
+/**
+ * HEIG-VD IOT Project
+ * Auteurs:
+ * Anthony Coke
+ * Guilain Mbayo
+ * Mehdi Salhi
+ */
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
 void setup() {
   // put your setup code here, to run once:
+    //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
 }
 
 void loop() {
