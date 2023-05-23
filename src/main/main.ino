5ba6444feaf717d5976c2fb7d60f5bb232fe46d2
Mbayo <gmbayo11@gmail.com>
2023-05-23 16:30:04 +0200

    Add web server in arduino with poc of api

--- src/main/main.ino
+++ src/main/main.ino
@@ -8,13 +8,16 @@
 
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <Arduino_MKRIoTCarrier.h>
 
-#define SECRET_SSID ""
-#define SECRET_PASS ""
+#define SECRET_SSID "Ancalagon"
+#define SECRET_PASS "actuelle"
 
+MKRIoTCarrier carrier; 
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
+WiFiServer server(80);
 
 void setup() {
   // put your setup code here, to run once:
@@ -46,12 +49,99 @@ void setup() {
     // wait 10 seconds for connection:
     delay(10000);
   }
-
+  server.begin();
   // you're connected now, so print out the data:
   Serial.print("You're connected to the network");
+  printWifiStatus();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  listenClients();
+}
 
+void listenClients(){
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an HTTP request ends with a blank line
+    boolean currentLineIsBlank = true;
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the HTTP request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard HTTP response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 20");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          
+          // Send the datas here
+          
+          client.println("<br />");
+          
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+          currentLine = "";
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+          currentLine += c;
+        }
+
+        if (currentLine.endsWith("GET /test")) {
+               // send a standard HTTP response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 20");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // Send the datas here
+          client.print("My test");
+          client.println("<br />");
+          
+          client.println("</html>");
+          break;
+        }
+
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
