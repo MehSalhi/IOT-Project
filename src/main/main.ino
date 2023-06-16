718cc2db7bc22efe375f95f2c0bc0b93e7b02db3
Mbayo <gmbayo11@gmail.com>
2023-06-16 11:41:40 +0200

    Clean arduino code and separate credentials from the main code

--- src/main/main.ino
+++ src/main/main.ino
@@ -11,51 +11,41 @@
 #include <Arduino_MKRIoTCarrier.h>
 #include <ArduinoJson.h>
 #include <ArduinoMqttClient.h>
-
-#define SECRET_SSID "."
-#define SECRET_PASS "hqh97n8ircpb5bj"
-
-//#define SECRET_SSID "Ancalagon"
-//#define SECRET_PASS "actuelle"
+#include "secret.h"
 
 #define fanON   255
 #define fanOFF  0
 #define fanPIN  A3
+#define moistPin A6
 
 MKRIoTCarrier carrier; 
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+char ssid[] = SECRET_SSID;       // your network SSID (name)
+char pass[] = SECRET_PASS;       // your network password 
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
-
-String location = "serre_1";
+IPAddress commander(192,168,9,194); //your central server address
 
 const char broker[] = "192.168.9.194";
-//const char broker[] = "test.mosquitto.org";
 int        port     = 1883;
 
+String location = "serre_1";
 String topicSub  = "commander/devices/"; 
 String topicPub  = "arduino";
-
+String confTopic = "";
 String deviceUID = "";
-
-#define moistPin A6
 
 // Sensors
 #define tempIOTC
 #define humiIOTC
 #define lighIOTC
 #define humiSOIL
-#define movePIR
 
 int r, g, b, light;
-int pir;
 
-IPAddress commander(192,168,9,194); //your central server address
-
-//set interval for sending messages (milliseconds)
-// TODO ajouter les intervals pour chaque capteur individuel
+//set initial interval for sending messages (milliseconds)
 long interval = 20000;
 unsigned long previousMillis = 0;
 int count = 0;
@@ -63,8 +53,8 @@ int count = 0;
 DynamicJsonDocument config(1024);
 
 void setup() {
-  // put your setup code here, to run once:
-    //Initialize serial and wait for port to open:
+  
+  //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -78,15 +68,12 @@ void setup() {
   delay(1000);
   carrier.display.setTextSize(3);
 
-  pir = carrier.getBoardRevision() == 1 ? A5 : A0;
   carrier.display.setRotation(0);
   delay(1500);
-  pinMode(pir, INPUT);
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
     while (true);
   }
 
@@ -134,16 +121,20 @@ void setup() {
   Serial.println("You're connected to the network");
   printWifiStatus();
 
+  // device initial configuration
   config["deviceUID"] = deviceUID;
   config["sensors"] = "[\"tempIOTC\", \"humiIOTC\", \"lighIOTC\", \"humiSOIL\", \"movePIR\"]";
   config["measurement interval"] = interval;
   config["deviceLocation"] = "serre_1";
   config["actions"] = "";
 
-  String confTopic = "commander/devices/" + deviceUID;
+  confTopic = "commander/devices/" + deviceUID;
   String conf;
   serializeJson(config, conf);
+
+  // publish the initial configuration
   publishTopics(confTopic, conf);
+
   // set the message receive callback
   mqttClient.onMessage(listenTopics);
 
@@ -153,13 +144,10 @@ void setup() {
 
   // subscribe to a topic
   mqttClient.subscribe(topicSub);
-  //topicPub = "commander/devices/32203593719188/update";
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   // call poll() regularly to allow the library to send MQTT keep alive which
-
   // avoids being disconnected by the broker
   mqttClient.poll();
 
@@ -169,10 +157,10 @@ void loop() {
     // save the last time a message was sent
     previousMillis = currentMillis;
     sendData();
-    //getProx();
   }
 }
 
+// callback function that listen to an MQTT topic
 void listenTopics(int messageSize){
   String topic = mqttClient.messageTopic();
   String subTop = "";
@@ -188,10 +176,10 @@ void listenTopics(int messageSize){
   
   while (mqttClient.available()) {
     content += String((char)mqttClient.read());
-    //Serial.println(content);
   }
   Serial.println(content);
   
+  // Decompose the topic
   do{
     if(index_2 == 0){
       index_1 = topic.indexOf('/', 0);
@@ -202,11 +190,9 @@ void listenTopics(int messageSize){
       index_2 = topic.indexOf('/', index_1 + 1);
       subTop = topic.substring(index_1 + 1, index_2);
     }
-    Serial.println(String(index_1) + " : " + index_2);
-    Serial.println(subTop);
 
     if(subTop == String("update")){
-      Serial.println(content);
+      // read the action and the new interval and set it in the configuration
       deserializeJson(config, content);
       if(config["actions"] == "ON"){
         Serial.println("fan ON");        
@@ -214,10 +200,13 @@ void listenTopics(int messageSize){
       } else if(config["actions"] == "OFF"){
         Serial.println("fan OFF");
         fanCtrl(fanOFF);
-      } else {Serial.println("Nothing to see...");} // Add features here
+      } else {Serial.println("Nothing to see...");} // Add new features here
       interval = config["measurement interval"];      
 
-      // TODO Send updated config
+      // send the updated config
+      String conf;
+      serializeJson(config, conf);
+      publishTopics(confTopic, conf);
     } else if (subTop == "test"){
       Serial.println("This is a test");
     }
@@ -228,6 +217,7 @@ void listenTopics(int messageSize){
   Serial.println();
 }
 
+// publish a string on an MQTT topic
 void publishTopics(String topic, String value){
   Serial.print("Sending message to topic: ");
   Serial.println(topic);
@@ -241,6 +231,7 @@ void publishTopics(String topic, String value){
   delay(500);
 }
 
+// print the status of the connected wifi
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -265,68 +256,34 @@ void printWifiStatus() {
 
 void sendData() {
   // data that will be formatted and sent to the server
-
-  // counts the number of sensors
-  unsigned int sensorCount = 0;  
-
   // append sensor data conditionnaly
   
   #ifdef tempIOTC
-    //Serial.print("tempIOTC: ");
-    ++sensorCount;
     String temp = getTemp();
-    //Serial.println(temp);
-    // convert and format to string
     publishTopics(topicPub, formatLineProtocol("temperature",temp));
   #endif
   
   #ifdef humiIOTC
-    //Serial.print("humiIOTC: ");
-    ++sensorCount;
     String humi = getHumi();
-    //Serial.println(humi);
-    // convert and format to string
     publishTopics(topicPub, formatLineProtocol("humidity", humi));
   #endif
 
   #ifdef lighIOTC
-    //Serial.print("lighIOTC: ");
-    ++sensorCount;
     getLight();
-    //Serial.println(light);
-    // convert and format to string
     publishTopics(topicPub, formatLineProtocol("light", String(light)));
   #endif
 
   #ifdef humiSOIL
-    //Serial.print("humiSOIL: ");
-    ++sensorCount;
     String humiSoil = getSoilHum();
-    //Serial.println(humiSoil);
-    // convert and format to string
     publishTopics(topicPub, formatLineProtocol("soil_humidity", humiSoil));
   #endif
 
-  #ifdef movePIR
-    //Serial.print("movePIR: ");
-    ++sensorCount;    
-    int prox = getProx();
-    //Serial.println(prox);
-    // convert and format to string
-    //data["prox"] = prox;
-  #endif
-
-  // TODO: move out of this func
-  // should be used to display the return of getData()
+  // should be used to display the return of getData() on the MKR screen
   carrier.display.setCursor(0, 0);
   carrier.display.fillScreen(ST77XX_BLACK);
-  carrier.display.print("tempIOTC\n" + String(temp) + "\n" + "humiIOTC\n" + humi + "\n" + "lighIOTC\n" + light + "\n" + "humiSOIL\n" + humiSoil + "\n" + "movePIR\n" + prox);
+  carrier.display.print("tempIOTC\n" + String(temp) + "\n" + "humiIOTC\n" + humi + "\n" + "lighIOTC\n" + light + "\n" + "humiSOIL\n" + humiSoil + "\n");
   
 }
-
-// package data to send
-
-// send data
 
 // get temperature from IOT Carrier
 String getTemp() {
@@ -350,24 +307,17 @@ void getLight() {
 // get humidity from SIL sensor
 String getSoilHum() {
   int raw_moisture = analogRead(moistPin);
-  Serial.print("HUMI SOIL: ");  
-  Serial.println(raw_moisture);
   return String(map(raw_moisture, 0, 400, 0, 100));
 }
 
-// get movement from PIR sensor
-int getProx() {
-  Serial.print("prox: ");
-  Serial.println(digitalRead(pir));
-  return digitalRead(pir);
-}
-
+// format a measurement and its value to line protocol format
 String formatLineProtocol(String measurement, String value){
   String result = measurement;
   result += ",device=" + deviceUID + ",location=" + location + " value=" + value + "";
   return result;
 }
 
+// Activate or deactivate the fan
 void fanCtrl(int val) {
   analogWrite(fanPIN, val);
 }
---
+++ src/main/secret.h
@@ -0,0 +1,10 @@
+/**
+ * HEIG-VD IOT Project
+ * Auteurs:
+ * Anthony Coke
+ * Guilain Mbayo
+ * Mehdi Salhi
+ */
+
+#define SECRET_SSID ""
+#define SECRET_PASS ""
